6fa059b5fa6378a87c997c385846fd055dcd390b
Doug <doug.axtell@gmail.com>
2015-05-11 16:51:30 -0400

    can now parse talk pages, but can't yet insert into ElasticSearch

--- GC_Search/FetchAllTalksForOneGC.py
+++ GC_Search/FetchAllTalksForOneGC.py
@@ -15,20 +15,29 @@ class FetchTalks:
         talkTag = '<span class="talk">'
         talkTag_index = line.find(talkTag)
         if talkTag_index != -1:
+            ahrefIndex = line.find('<a href=', talkTag_index + len(talkTag))
+            if ahrefIndex == -1:
+                return None
             linkStart_index = line.find('"', talkTag_index + len(talkTag))
             linkEnd_index = line.find('"', linkStart_index + 1)
             return(line[linkStart_index + 1:linkEnd_index])
         else:
             return None
                 
-    def _FindTalkLinksInPage(self, pageHandle):
+    def _FindConfIdAndTalkLinksInPage(self, pageHandle):
+        confId = ''
         talkLinks = []
+        titleFound = False
         for line in pageHandle:
             str_line = str(line)
+            if titleFound == False:
+                confId = self._FindConfId(str_line)
+                if confId != '':
+                    titleFound = True
             str_link = self._GetTalkLink(str_line)
             if str_link != None:
                 talkLinks.append(str_link)
-        return talkLinks
+        return ( confId, talkLinks )
 
     def _FetchWeekenSummaryPage(self, url):
         return self._FetchPage(url)
@@ -36,18 +45,32 @@ class FetchTalks:
     def _FetchIndividualTalk(self, url):
         return self._FetchPage(url)
 
+    def _FindConfId(self, strLine):
+        confId = ''
+        titleOpenTag = '<title>'
+        titleIndex = strLine.find(titleOpenTag)
+        if titleIndex != -1:
+            titleString = HtmlTagParser.GetTagContents(titleOpenTag, strLine)
+            titleSegments = titleString.split(' ')
+            confId = titleSegments[0] + ' ' + titleSegments[1]
+            titleFound = True
+        return confId
+
     def FetchTalks(self, weekendUrl):
         handlesToTalks = []
         summaryPageHandle = self._FetchWeekenSummaryPage(weekendUrl)
-        linksToTalks = self._FindTalkLinksInPage(summaryPageHandle)
+        confId, linksToTalks = self._FindConfIdAndTalkLinksInPage(summaryPageHandle)
+        print('num talks: ' + str(len(linksToTalks)))
         for link in linksToTalks:
+            print(link)
             handlesToTalks.append(self._FetchIndividualTalk(link))
-        return handlesToTalks
+        return (confId, handlesToTalks)
 
 
 class FetchTalksTest(unittest.TestCase):
 
     htmlContent = """<html>
+                    <title>October 2014 LDS General Conference Talks</title>
                     <table>
                     <tr>
                     <td>
@@ -61,23 +84,42 @@ class FetchTalksTest(unittest.TestCase):
                     <span class="speaker">By President Boyd K. Packer</span>
                     </td>
                     </tr>
+                    <tr>
+                    <td>
+                    <span class="talk">Video Presentation: The Holy Temple</span>
+                    <span class="speaker">The Church of Jesus Christ of Latter-day Saints</span>
+                    </td>
+                    </tr>
                     </table>
                     </html>"""
+
+    confId = 'October 2014'
 
     def setUp(self):
         self.ft = FetchTalks()
 
-    def test_FetchPage_OneUrlRequestMade(self):
+    def test_FetchWeekenSummaryPage_OneUrlRequestMade(self):
         talkUrl = 'https://www.lds.org/general-conference/2014/10/welcome-to-conference?lang=eng'
         with patch.object(urllib.request, 'urlopen', return_value=None) as mock_method:
             self.ft._FetchWeekenSummaryPage(talkUrl)
         mock_method.assert_called_once_with(talkUrl)
 
-    def test_FindTalkLinksInPage_AllTalkLinksFound(self):
-        linksToTalks = self.ft._FindTalkLinksInPage(io.StringIO(self.htmlContent))
+    def test_FetchPage_OneUrlRequestMade(self):
+        talkUrl = 'https://www.lds.org/general-conference/2014/10/welcome-to-conference?lang=eng'
+        with patch.object(urllib.request, 'urlopen', return_value=None) as mock_method:
+            self.ft._FetchIndividualTalk(talkUrl)
+        mock_method.assert_called_once_with(talkUrl)
+
+    def test_FindConfId_TitleTagSearched(self):
+        confId = self.ft._FindConfId('<title>October 2014 LDS General Conference Talks</title>')
+        self.assertEqual(confId, self.confId)
+
+    def test_FindConfIdAndTalkLinksInPage_AllTalkLinksFound(self):
+        confId, linksToTalks = self.ft._FindConfIdAndTalkLinksInPage(io.StringIO(self.htmlContent))
         self.assertEqual(len(linksToTalks), 2)
         self.assertEqual(linksToTalks[0], 'https://www.lds.org/general-conference/2014/10/welcome-to-conference?lang=eng')
         self.assertEqual(linksToTalks[1], 'https://www.lds.org/general-conference/2014/10/the-reason-for-our-hope?lang=eng')
+        self.assertEqual(confId, self.confId)
 
     def test_FetchTalks_ReqSummaryParseItThenReqAllTalks(self):
         weekendUrl = 'https://www.lds.org/general-conference/sessions/2014/10?lang=eng'
@@ -85,9 +127,32 @@ class FetchTalksTest(unittest.TestCase):
         talkLinks = []
         talkLinks.append('https://www.lds.org/general-conference/2014/10/welcome-to-conference?lang=eng')
         self.ft._FetchWeekenSummaryPage = Mock(return_value=wHandle)
-        self.ft._FindTalkLinksInPage = Mock(return_value=talkLinks)
+        self.ft._FindConfId = Mock(return_value=self.confId)
+        self.ft._FindConfIdAndTalkLinksInPage = Mock(return_value=( self.confId, talkLinks ))
         self.ft._FetchIndividualTalk = Mock(return_value=io.StringIO('some talk\'s content'))
         self.ft.FetchTalks(weekendUrl)
         self.ft._FetchWeekenSummaryPage.assert_called_with(weekendUrl)
-        self.ft._FindTalkLinksInPage.assert_called_once_with(wHandle)
+        self.ft._FindConfIdAndTalkLinksInPage.assert_called_once_with(wHandle)
         self.ft._FetchIndividualTalk.assert_called_with(talkLinks[0])
+
+
+class HtmlTagParser:
+
+    def GetTagContents(tag, line, tagIndex = -1):
+        contents = ''
+        if tagIndex == -1:
+            tagIndex = line.find(tag)
+        if tagIndex != -1:
+            tagStartIndex = tagIndex + len(tag)
+            tagEndIndex = line.find('<', tagStartIndex + 1)
+            contents = line[tagStartIndex:tagEndIndex]
+        return contents
+
+
+class HtmlTagParserTest(unittest.TestCase):
+
+    def test_GetTagContents(self):
+        htmlStr = '<title>October 2014 LDS General Conference Talks</title>'
+        tag = '<title>'
+        tagContent = HtmlTagParser.GetTagContents(tag, htmlStr)
+        self.assertEqual(tagContent, 'October 2014 LDS General Conference Talks')
--- GC_Search/InsertGCTalksIntoES.py
+++ GC_Search/InsertGCTalksIntoES.py
@@ -4,29 +4,91 @@ from unittest.mock import Mock
 from elasticsearch import Elasticsearch
 import io
 from FetchAllTalksForOneGC import FetchTalks
+from FetchAllTalksForOneGC import HtmlTagParser
+import json
 
 
 
 class IndexTalks:
 
+    index_name = 'gc-index'
+    doc_type = 'talk'
+
     def __init__(self):
         self.ft = FetchTalks()
+        self.es = Elasticsearch()
+        self.es_id_seq = 0
+        self.confId = ''
 
     def FetchTalksAndIndexThem(self, url):
-        talkHandles = self.ft.FetchTalks(url)
-        ##for handle in talkHandles:
-            
+        self.confId, talkHandles = self.ft.FetchTalks(url)
+        print(str.format('confId: {}, num talk hanldes: {}', self.confId, len(talkHandles)))
+        for handle in talkHandles:
+            self._InsertOneTalkIntoES(handle)
+
+    def _GetNextId(self):
+        result = self.es_id_seq
+        self.es_id_seq = self.es_id_seq + 1
+        return result
+
+    def _GetTitleAndAuthor(self, line, tag, tagIndex):
+        titleString = HtmlTagParser.GetTagContents(tag, line, tagIndex)
+        titleSegments = titleString.split(' - By ')
+        title = titleSegments[0]
+        author = titleSegments[1]
+        return ( title, author )
+
+    def _GetTitleAuthorContent(self, talkHandle):
+        title = ''
+        author = ''
+        titleOpenTag = '<title>'
+        titleFound = False
+        talkContent = ''
+        for line in talkHandle:
+            strLine = str(line)
+            talkContent = talkContent + strLine
+            if titleFound == False:
+                titleIndex = strLine.find(titleOpenTag)
+                if titleIndex != -1:
+                    title, author = self._GetTitleAndAuthor(strLine, titleOpenTag, titleIndex)
+                    titleFound = True
+        return ( title, author, talkContent )
+
+    def _InsertOneTalkIntoES(self, talkHandle):
+        title, author, talkContent = self._GetTitleAuthorContent(talkHandle)
+        json_body = json.dumps({'title': title, 'author': author, 'ConfId': self.confId, 'content': talkContent})
+        idnum = self._GetNextId()
+        print('indexing doc num: ' + str(idnum))
+        self.es.index(self.index_name, self.doc_type, idnum, json_body)
+
 
 
 class IndexTalksTest(unittest.TestCase):
 
+    htmlContent = """<html>
+                    <title>Welcome to Conference - By President Thomas S. Monson</title>
+                    </html>"""
+
+    confId = 'October 2014'
+
     def setUp(self):
         self.it = IndexTalks()
+
+    def test_InsertOneTalkIntoES_ESApiCalled(self):
+        json_body = json.dumps({'title': 'Welcome to Conference', 'author': 'President Thomas S. Monson', 'ConfId': self.confId, 'content': self.htmlContent})
+        talkHandle = io.StringIO(self.htmlContent)
+        self.it.confId = self.confId
+        with patch.object(Elasticsearch, 'index', return_value=None) as mock_method:
+            self.it._InsertOneTalkIntoES(talkHandle)
+        mock_method.assert_called_once_with(self.it.index_name, self.it.doc_type, self.it._GetNextId()-1, json_body)
 
     def test_FetchTalks_IndexThemOneByOne(self):
         talkUrl = 'https://www.lds.org/general-conference/2014/10/welcome-to-conference?lang=eng'
         talkHandles = []
         talkHandles.append(io.StringIO('some talk\'s content'))
-        self.it.ft.FetchTalks = Mock(return_value=talkHandles)
+        self.it.ft.FetchTalks = Mock(return_value= ( self.confId, talkHandles ))
+        self.it._InsertOneTalkIntoES = Mock(return_value=None)
         self.it.FetchTalksAndIndexThem(talkUrl)
         self.it.ft.FetchTalks.assert_called_once_with(talkUrl)
+        self.it._InsertOneTalkIntoES.assert_called_once_with(talkHandles[0])
+        
