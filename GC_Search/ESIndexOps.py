97e89a2696626f5fe0faba9bfe0bb755414bd383
Doug <doug.axtell@gmail.com>
2015-07-07 14:15:14 -0400

    notes.txt and ESIndexOps.py (which does not yet work)

---
+++ GC_Search/ESIndexOps.py
@@ -0,0 +1,53 @@
+import unittest
+from unittest.mock import patch
+from unittest.mock import Mock
+from elasticsearch import Elasticsearch
+import json
+
+
+class ESIndexOps:
+
+    def __init__(self):
+        self.es = Elasticsearch()
+        self.index_config = {
+                { 'settings' : {
+                    'analysis' : {
+                        'analyzer' : {
+                            'remove_html' : {
+                                'char_filter' : [ 'html_strip' ]
+                                }
+                            }
+                        }
+                    }
+                }
+                ,
+                {
+                    'mappings' : {
+                        'talk' : {
+                            'properties' : {
+                                'title' : { 'type': 'string' },
+                                'author' : { 'type': 'string' },
+                                'confid' : { 'type': 'string' },
+                                'content' : { 'type': 'string', 'analyzer': 'remove_html' }
+                            }
+                        }
+                    }
+                }
+            }
+        self.index_name = 'gc_index'
+        
+
+    def CreateGCIndexWithMapping(self):
+        index_json = json.dumps(self.index_config)
+        self.es.create(index=self.index_name, body=index_json)
+
+
+class ESIndexOpsTests(unittest.TestCase):
+
+    def setUp(self):
+        self.esio = ESIndexOps()
+
+    def test_CreateGCIndexWithMapping_ESCalledAppropriately(self):
+        with patch.object(Elasticsearch, 'create', return_value=None) as mock_method:
+            self.esio.CreateGCIndexWithMapping()
+        mock_method.assert_called_once_with(index=self.esio.index_name, body=json.dumps(self.esio.mapping))
