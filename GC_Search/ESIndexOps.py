06bc0f4e56cfbe41710537b9d751a9fe2459211b
Doug <doug.axtell@gmail.com>
2015-07-09 14:12:46 -0400

    succeeded in explicit programmatic index creation and html char strip is working

--- GC_Search/ESIndexOps.py
+++ GC_Search/ESIndexOps.py
@@ -10,36 +10,37 @@ class ESIndexOps:
     def __init__(self):
         self.es = Elasticsearch()
         self.index_config = {
-                { 'settings' : {
-                    'analysis' : {
-                        'analyzer' : {
-                            'remove_html' : {
-                                'char_filter' : [ 'html_strip' ]
+                 "settings" : {
+                    "analysis" : {
+                        "analyzer" : {
+                            "remove_html" : {
+                                "tokenizer" : "standard",
+                                "char_filter" : [ "html_strip" ]
                                 }
                             }
                         }
                     }
-                }
+                
                 ,
-                {
-                    'mappings' : {
-                        'talk' : {
-                            'properties' : {
-                                'title' : { 'type': 'string' },
-                                'author' : { 'type': 'string' },
-                                'confid' : { 'type': 'string' },
-                                'content' : { 'type': 'string', 'analyzer': 'remove_html' }
+                
+                    "mappings" : {
+                        "talk" : {
+                            "properties" : {
+                                "title" : { "type": "string" },
+                                "author" : { "type": "string" },
+                                "confid" : { "type": "string" },
+                                "content" : { "type": "string", "analyzer": "remove_html" }
                             }
                         }
                     }
-                }
+                
             }
-        self.index_name = 'gc_index'
+        self.index_name = "gc-index"
         
 
     def CreateGCIndexWithMapping(self):
         index_json = json.dumps(self.index_config)
-        self.es.create(index=self.index_name, body=index_json)
+        self.es.indices.create(index=self.index_name, body=index_json)
 
 
 class ESIndexOpsTests(unittest.TestCase):
@@ -48,6 +49,6 @@ class ESIndexOpsTests(unittest.TestCase):
         self.esio = ESIndexOps()
 
     def test_CreateGCIndexWithMapping_ESCalledAppropriately(self):
-        with patch.object(Elasticsearch, 'create', return_value=None) as mock_method:
+        with patch.object(self.esio.es.indices, "create", return_value=None) as mock_method:
             self.esio.CreateGCIndexWithMapping()
-        mock_method.assert_called_once_with(index=self.esio.index_name, body=json.dumps(self.esio.mapping))
+        mock_method.assert_called_once_with(index=self.esio.index_name, body=json.dumps(self.esio.index_config))
