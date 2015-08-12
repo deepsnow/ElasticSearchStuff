b770cf5a010898ff8c02eef09724e4d37f10ebf6
deepsnow <doug.axtell@gmail.com>
2015-08-12 16:04:26 -0400

    successful loading of elasticsearch.angular.js, but still working on getting first round trip of search query from JS to ES and back...

--- GC_Search/web/es.js
+++ GC_Search/web/es.js
@@ -1,12 +1,32 @@
-angular.module('gcApp', [])
-.controller('gcEsSearch', function($scope) {
+angular.module('gcApp', ['elasticsearch'])
+.service('client', function (esFactory) {
+	return esFactory({
+		host: 'localhost:9200',
+		log: 'trace'
+})})
+.controller('gcEsSearch', function($scope, client, esFactory) {
 	
-	$scope.searcher = {};
+	$scope.searcher = this;
 	$scope.searcher.searchTerm = "enter search term";
 	$scope.searcher.results = [];
 	
-	$scope.searcher.search = function () {
-		
+	$scope.searcher.search = function ($scope, client) {
+		client.search({
+			index: 'gc',
+			type: 'talk',
+			body: {
+				query: {
+					match: {
+						content: $scope.searcher.searchTerm
+					}
+				}
+			}
+		}).then(function (resp) {
+			$scope.searcher.results = resp.hits.hits;
+			console.trace('successful query?');
+		}, function (err) {
+			console.trace(err.message);
+		});
 	};
 	
 });
