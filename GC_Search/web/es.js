c4869ae3ad68a65b33c5fc715ff1ffec0a9df5d2
deepsnow <doug.axtell@gmail.com>
2015-08-24 18:22:06 -0400

    big improvements today in displaying hits, but still a number of obstacles to overcome and bugs to sort out (see notes)

--- GC_Search/web/es.js
+++ GC_Search/web/es.js
@@ -4,7 +4,7 @@ angular.module('gcApp', ['elasticsearch'])
 		host: 'localhost:9200',
 		log: 'trace'
 })})
-.controller('gcEsSearch', function($scope, client, esFactory) {
+.controller('gcEsSearch', function($scope, client, esFactory, $sce) {
 	
 	$scope.searcher = this;
 	$scope.searcher.searchTerm = "enter search term";
@@ -16,14 +16,28 @@ angular.module('gcApp', ['elasticsearch'])
 			index: 'gc',
 			type: 'talk',
 			body: {
+				fields : ['title', 'author', 'confid', 'url'],
 				query: {
 					match: {
 						content: $scope.searcher.searchTerm
+					}
+				},
+				highlight : {
+					  fields : {
+						content : {number_of_fragments: 50}
 					}
 				}
 			}
 		}).then(function (resp) {
 			$scope.searcher.results = resp.hits.hits;
+			
+			for (var i=0; i<$scope.searcher.results.length; i++) {
+				var result = $scope.searcher.results[i]
+				for (var j=0; j<result.highlight.content.length; j++) {
+					result.highlight.content[j] = $sce.trustAsHtml(result.highlight.content[j])
+				}
+			}
+			
 			console.trace($scope.searcher.results.length);
 		}, function (err) {
 						
