1a06092ca6f160f49f5eecbe8b2d654e2d43c388
deepsnow <doug.axtell@gmail.com>
2015-08-31 17:33:11 -0400

    can now fetch all results (not just first 10), but still need to do a lot of cleanup and additional expansion

--- GC_Search/web/es.js
+++ GC_Search/web/es.js
@@ -6,48 +6,69 @@ angular.module('gcApp', ['elasticsearch'])
 })})
 .controller('gcEsSearch', function($scope, client, esFactory, $sce) {
 	
-	$scope.searcher = this;
-	$scope.searcher.searchTerm = "enter search term";
-	$scope.searcher.results = [];
+	$scope.searcher = this
+	$scope.searcher.searchTerm = "enter search term"
+	$scope.searcher.results = []
 	
 	$scope.searcher.search = function () {
-		
-		client.search({
+			
+		client.count({
 			index: 'gc',
 			type: 'talk',
 			body: {
-				fields : ['title', 'author', 'confid', 'url'],
 				query: {
 					match: {
 						content: $scope.searcher.searchTerm
 					}
-				},
-				highlight : {
-					  fields : {
-						content : {number_of_fragments: 50}
-					}
 				}
 			}
 		}).then(function (resp) {
-			$scope.searcher.results = resp.hits.hits;
-			
-			for (var i=0; i<$scope.searcher.results.length; i++) {
-				var result = $scope.searcher.results[i]
-				for (var j=0; j<result.highlight.content.length; j++) {
-					result.highlight.content[j] = $sce.trustAsHtml(result.highlight.content[j])
+			hitCount = resp.count
+
+			client.search({
+				index: 'gc',
+				type: 'talk',
+				size: hitCount,
+				body: {
+					fields : ['title', 'author', 'confid', 'url'],
+					query: {
+						match: {
+							content: $scope.searcher.searchTerm
+						}
+					},
+					highlight : {
+						  fields : {
+							content : {number_of_fragments: 50}
+						}
+					}
 				}
-			}
-			console.trace(resp.hits.total)
-			console.trace($scope.searcher.results.length)
+			}).then(function (resp) {
+				$scope.searcher.results = resp.hits.hits
+				
+				for (var i=0; i<$scope.searcher.results.length; i++) {
+					var result = $scope.searcher.results[i]
+					for (var j=0; j<result.highlight.content.length; j++) {
+						result.highlight.content[j] = $sce.trustAsHtml(result.highlight.content[j])
+					}
+				}
+				console.trace(resp.hits.total)
+				console.trace($scope.searcher.results.length)
+			}, function (err) {
+							
+				if (err instanceof esFactory.errors.NoConnections) {
+				  $scope.error = new Error('Unable to connect to elasticsearch to fetch previously counted hits. ' +
+					'Make sure that it is running and listening at http://localhost:9200')
+				}
+				console.trace(err.message);
+			})
+			
 		}, function (err) {
 						
 			if (err instanceof esFactory.errors.NoConnections) {
-			  $scope.error = new Error('Unable to connect to elasticsearch. ' +
-				'Make sure that it is running and listening at http://localhost:9200');
+			  $scope.error = new Error('Unable to connect to elasticsearch for counting search hits. ' +
+				'Make sure that it is running and listening at http://localhost:9200')
 			}
-			console.trace(err.message);
-		
-		});
-	};
-	
-});
+			console.trace(err.message)
+		})
+	}
+})
